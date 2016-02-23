5ef44e4a328e5d8932b19a484d1a248ed4b51526
oberegovskyi <oberegovskyi@i.ua>
2016-02-23 14:52:17 +0200

    Update App.java

--- src/main/java/Seeker/Seeker/App.java
+++ src/main/java/Seeker/Seeker/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) {
     	System.setProperty("webdriver.chrome.driver", "c:\\chromedriver.exe");
-		Viewer barym = new Viewer("Revenger","kokokoko1964", "Barym");
+		Viewer barym = new Viewer("****","****", "****");
 		barym.start();
     }
 }
